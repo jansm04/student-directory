7c53692e549648930c40ba003c3bf2cc325b9d54
jansm04 <jan.smailbegovic@gmail.com>
2024-05-24 10:49:49 -0400

    clean up code

--- app/src/main/java/com/jan/studentdirectory/Student.java
+++ app/src/main/java/com/jan/studentdirectory/Student.java
@@ -20,48 +20,19 @@ public class Student {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getStudentId() {
         return studentId;
     }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
     public String getAddress() {
         return address;
     }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public double getLatitude() {
         return latitude;
     }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLongitude() {
         return longitude;
     }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 }
