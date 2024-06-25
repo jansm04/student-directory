2b1d2776b0f8140899f42c1dee8d7fdb45e62679
jansm04 <jan.smailbegovic@gmail.com>
2024-06-25 12:43:22 -0400

    fix bug in cache manager that prevented service from being paused

--- app/src/main/java/com/jan/studentdirectory/cache/CacheManager.java
+++ app/src/main/java/com/jan/studentdirectory/cache/CacheManager.java
@@ -13,14 +13,12 @@ public class CacheManager {
 
     private final SQLiteManager sqlManager;
     private final List<Student> students;
-    private final Timer cacheTimer;
-    private final Timer clearTimer;
+    private Timer cacheTimer;
+    private Timer clearTimer;
 
     public CacheManager(SQLiteManager sqlManager, List<Student> students) {
         this.sqlManager = sqlManager;
         this.students = students;
-        this.cacheTimer = new Timer();
-        this.clearTimer = new Timer();
     }
 
     public void startCachingInterval(long seconds) throws InvalidTimeException {
@@ -28,6 +26,7 @@ public class CacheManager {
             throw new InvalidTimeException("Caching interval must be a non-negative integer.");
         }
         long milliseconds = seconds * 1000;
+        cacheTimer = new Timer();
         cacheTimer.schedule(new CacheTask(sqlManager, students), 0, milliseconds);
     }
 
@@ -36,6 +35,7 @@ public class CacheManager {
             throw new InvalidTimeException("Clearing interval must be a non-negative integer.");
         }
         long milliseconds = seconds * 1000;
+        clearTimer = new Timer();
         clearTimer.schedule(new ClearTask(sqlManager), 1000, milliseconds);
     }
 
